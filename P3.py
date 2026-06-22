2ee1fec8b2d624affef95e9630e15db385faeb23
Adina Halter <haltersweb@gmail.com>
2026-06-22 19:19:46 -0400

    streamlined a bit and added time trial

--- P3.py
+++ P3.py
@@ -31,42 +31,52 @@ def is_prime(n: int) -> bool:
 
 # Solve brute force as a factor tree
 def largest_prime_brute(n: int) -> int:
-    # edge cases: 
     if n <= 1: # edge 1: n is too small to be a prime
         print(f"n of {n} too small to be a prime")
         return -1
     if is_prime(n): # edge 2: n already is prime
         return n
     quotient = n # start with n
-    while quotient % 2 == 0: # factor out all evens
-        quotient = int(quotient / 2)
+    i = 2 # only even number to test
+    while quotient % i == 0: # factor out all evens
+        quotient = int(quotient / i)
         if quotient == 1:
-            return 2
-        if is_prime(quotient): # is the resultant quotient after looping a prime?
+            return i
+        if is_prime(quotient): # after i factored out is result prime?
             return quotient
-    # limit = quotient # TODO: try this later
-    for i in range(3, 10000, 2): # odds # TODO: using 10,000 limit as temporary range.  Change to while True
-        is_new_i = True # toggle to True when factoring same i again
-        if is_new_i and not is_prime(i):
+    i = 3 # first odd number to test
+    while True:
+        if not is_prime(i): # i isn't prime so:
+                i += 2 # next odd number
                 continue
-        is_new_i = False
         while quotient % i == 0:
             quotient = int(quotient / i)
             if quotient == 1:
                 return i
         if is_prime(quotient):
             return quotient
-        is_new_i = True
-    print(f"something went wrong and no largest prime factors found")
-    return -1
+        i += 2 # next odd number
 
-print(f"for n = 0 largest prime is:", largest_prime_brute(0)) #n too small
-print(f"for n = 1 largest prime is:", largest_prime_brute(1)) #n too small
-print(f"for n = 2 largest prime is:", largest_prime_brute(2)) #2
-print(f"for n = 3 largest prime is:", largest_prime_brute(3)) #3
-print(f"for n = 7 largest prime is:", largest_prime_brute(7)) #7
-print(f"for n = 8 largest prime is:", largest_prime_brute(8)) #2
-print(f"for n = 9 largest prime is:", largest_prime_brute(9)) #3
-print(f"for n = 390 largest prime is:", largest_prime_brute(390)) #13
-print(f"for n = 13195 largest prime is:", largest_prime_brute(13195)) #29
+# print(f"for n = 0 largest prime is:", largest_prime_brute(0)) #n too small
+# print(f"for n = 1 largest prime is:", largest_prime_brute(1)) #n too small
+# print(f"for n = 2 largest prime is:", largest_prime_brute(2)) #2
+# print(f"for n = 3 largest prime is:", largest_prime_brute(3)) #3
+# print(f"for n = 7 largest prime is:", largest_prime_brute(7)) #7
+# print(f"for n = 8 largest prime is:", largest_prime_brute(8)) #2
+# print(f"for n = 9 largest prime is:", largest_prime_brute(9)) #3
+# print(f"for n = 390 largest prime is:", largest_prime_brute(390)) #13
+# print(f"for n = 13195 largest prime is:", largest_prime_brute(13195)) #29
+# print(f"for n = 600851475143 largest prime is:", largest_prime_brute(600851475143)) #6857
+
+### ----------------- TIME THE EXECUTION ----------------- ###
+import time
+
+# TRIAL 1
+start_time = time.perf_counter_ns()
+
+# run the function
 print(f"for n = 600851475143 largest prime is:", largest_prime_brute(600851475143)) #6857
+
+# print time elapsed
+elapsed_ms = (time.perf_counter_ns() - start_time) / 1e6
+print(f"Execution time for TRIAL 1: {elapsed_ms:.5f} ms")
